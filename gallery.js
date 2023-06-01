09fd03b7cece00c9240ec194312859387c8c63e9
CaptainSkiddow <dekkerrens@outlook.com>
2023-06-01 13:49:02 +0200

    xx
    x

--- gallery.js
+++ gallery.js
@@ -16,4 +16,47 @@ gsap.from(".container", {
   })
 
 
+  gsap.from(".containerrow2", {
+    duration: 0.1,
+    x: 200,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".containerrow2",
+      markers: true,
+      end: "top 30%",
+      start: "top 50%",
+        //events: onEnter onLeave onEnterBack onLeaveBack
+      toggleActions:"restart complete reverse reset"
+    }
+  })
+
+  gsap.from(".containerrow3", {
+    duration: 0.1,
+    x: -200,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".containerrow3",
+      markers: true,
+      end: "top 30%",
+      start: "top 50%",
+        //events: onEnter onLeave onEnterBack onLeaveBack
+      toggleActions:"restart complete reverse reset"
+    }
+  })
+
+  gsap.from(".containerrow4", {
+    duration: 0.1,
+    x: 200,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".containerrow4",
+      markers: true,
+      end: "top 30%",
+      start: "top 50%",
+        //events: onEnter onLeave onEnterBack onLeaveBack
+      toggleActions:"restart complete reverse reset"
+    }
+  })
+
+
 
